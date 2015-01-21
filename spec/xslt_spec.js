c63814756664b61a12365e5cafc8dccc37ae364f
Benjamin Orozco <benoror@gmail.com>
2015-01-20 19:50:23 -0600

    ASYNC XSLT Tests

--- spec/xslt_spec.js
+++ spec/xslt_spec.js
@@ -6,24 +6,31 @@ describe("XSLT", function() {
 
 	var fs = require('fs'),
 		libxslt = require('libxslt'),
-		xml = fs.readFileSync('spec/fixtures/test.xml', 'utf8'),
-		xsl = fs.readFileSync('spec/fixtures/test.xsl', 'utf8'),
 		xml_result = fs.readFileSync('spec/fixtures/result.xml', 'utf8');
 
-	it("should run", function (done) {
-		libxslt.parse(xsl, function(err, stylesheet) {
-			expect(err).toBeFalsy();
-			done();
+	it("should parse XSL", function (done) {
+		fs.readFile('views/xsl/sitemap.xsl', 'utf8', function (err, xsl) {
+			libxslt.parse(xsl, function(err, stylesheet) {
+				expect(err).toBeFalsy();
+				done();
+			});
 		});
 	});
 
 	it("should transform XML", function (done) {
-		libxslt.parse(xsl, function(err, stylesheet) {
-			stylesheet.apply(xml, function(err, result) {
-				if(!err) {
-					expect(result).toEqual(xml_result);
-				}
-				done();
+		fs.readFile('spec/fixtures/test.xsl', 'utf8', function (err, xsl) {
+			libxslt.parse(xsl, function(err, stylesheet) {
+				expect(err).toBeFalsy();
+				fs.readFile('spec/fixtures/test.xml', 'utf8', function (err, xml) {
+					stylesheet.apply(xml, function(err, result) {
+						expect(err).toBeFalsy();
+						if(!err) {
+							expect(result).toEqual(xml_result);
+						}
+						console.log(result);
+						done();
+					});
+				});
 			});
 		});
 	});
