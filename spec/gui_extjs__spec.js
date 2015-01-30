08c543c1f3d80288530c29298e89254653231100
Benjamin Orozco <benoror@gmail.com>
2015-01-30 17:39:27 -0600

    Specs: ExtJS + 'lib_' prefix renaming

---
+++ spec/gui_extjs__spec.js
@@ -0,0 +1,21 @@
+/**
+ * GUI ExtJS Testing
+ *
+ * 1. Read ExtJS root
+ */
+var frisby = require('frisby');
+var	panax = require('../panax');
+
+var	hostname = panax.ui.config.hostname,
+	port = panax.ui.config.port,
+	url = 'http://' + hostname + ':' + port;
+
+/**
+ * Test Read ExtJS root
+ * (when logged in)
+ */
+frisby.create('Read ExtJS root')
+	.get(url + '/gui/extjs')
+	.expectStatus(200)
+	.expectHeaderContains('content-type', 'text/html')
+.toss()
