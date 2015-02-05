1e13a656868d06e685ac76286e3f0262b529853f
Benjamin Orozco <benoror@gmail.com>
2015-02-05 13:33:37 -0600

    PanaxJS 2nd round: getXML

--- routes/api_build.js
+++ routes/api_build.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var sql = require('mssql');
-var PanaxJS = require('../../PanaxJS/index');
-var panax = require('../config/panax');
+var PanaxJS = require('../../PanaxJS');
+var panax_config = require('../config/panax');
 
 var path = require('path');
 var fs = require('fs');
@@ -16,114 +15,98 @@ module.exports = router;
  *
  * Build Entity GUI
  */
-router.get('/', function read(req, res, next) {
+router.get('/', function build(req, res, next) {
 	if (!req.session.userId)
 		return next({message: "Error: Not logged in"});
 	if (!req.query.catalogName)
 		return next({message: "Error: No catalogName supplied"});
 
-	var output = (req.query.output || 'extjs').toLowerCase(); // ExtJS is default GUI Output
+	req.query.output = (req.query.output || 'extjs').toLowerCase(); // ExtJS is default GUI Output
 
-	if (output != 'extjs')
-		return next({message: "Error: Output '" + output + "' not supported"});
+	if (req.query.output != 'extjs')
+		return next({message: "Error: Output '" + req.query.output + "' not supported"});
 
-	sql.connect(panax.db.config, function (err) {
-		if (err)
-			return next(err);
+	var oPanaxJS = new PanaxJS(req.query);
 
-		var sql_req = new sql.Request();
+	oPanaxJS.set('userId', req.session.userId);
+	oPanaxJS.set('tableName', req.query.catalogName);
+	/**/oPanaxJS.set('output', req.query.output);
+	oPanaxJS.set('getData', (req.query.getData || '0'));
+	oPanaxJS.set('getStructure', (req.query.getStructure || '1'));
+	oPanaxJS.set('lang', (req.session.lang || 'DEFAULT'));
 
-		var oPanaxJS = new PanaxJS(req.query);
-		oPanaxJS.set('userId', req.session.userId);
-		oPanaxJS.set('tableName', req.query.catalogName);
-		oPanaxJS.set('output', output);
-		oPanaxJS.set('getData', (req.query.getData || '0'));
-		oPanaxJS.set('getStructure', (req.query.getStructure || '1'));
-		oPanaxJS.set('lang', (req.session.lang || 'DEFAULT'));
+	oPanaxJS.getXML(panax_config.db.config, function (err, xml) {
+		var xmlDoc = libxslt.libxmljs.parseXml(xml);
 
-		var sql_str = oPanaxJS.toSQLString();
+		if(!xmlDoc)
+			return next({message: "Error: Parsing XML"});
 
-		sql_req.query(sql_str, function (err, recordset) {
-			if (err)
-				return next(err);
+		var catalog = {
+			dbId: xmlDoc.root().attr("dbId").value(),
+			lang: xmlDoc.root().attr("lang").value(),
+			Table_Schema: xmlDoc.root().attr("Table_Schema").value(),
+			Table_Name: xmlDoc.root().attr("Table_Name").value(),
+			mode: xmlDoc.root().attr("mode").value(),
+			controlType: xmlDoc.root().attr("controlType").value()
+		};
 
-			console.info('# /api/build - sql_str: ' + sql_str);
+		var sLocation = path.join(
+			panax_config.ui.guis[req.query.output].root,
+			"cache", "app",
+			catalog.dbId,
+			catalog.lang,
+			catalog.Table_Schema,
+			catalog.Table_Name,
+			catalog.mode
+		);
 
-			var xml = recordset[0][''];
+		var sFileName = path.join(sLocation, catalog.controlType + '.js');
 
-			if(!xml)
-				return next({message: "Error: Missing Data XML"});
+		// ToDo: Use Async functions?
+		if(fs.existsSync(sFileName) && !req.query.rebuild) {
+			console.info('# /api/build - Already existing file: ' + sFileName);
+			res.json({
+				success: true,
+				action: "existing",
+				filename: sFileName,
+				catalog: catalog
+			});
+		} else {
+			if(fs.existsSync(sFileName)) {
+				fs.unlinkSync(sFileName);
+				console.info('# /api/build - Deleted file: ' + sFileName);
+			}
 
-			var xmlDoc = libxslt.libxmljs.parseXml(xml);
+			console.info('# /api/build - Building file: ' + sFileName);
 
-			if(!xmlDoc)
-				return next({message: "Error: Parsing XML"});
+			if(!fs.existsSync(sLocation)) {
+				mkdirp(sLocation);
+				console.info('# /api/build - Mkdirp folder: ' + sLocation);
+			}
 
-			var catalog = {
-				dbId: xmlDoc.root().attr("dbId").value(),
-				lang: xmlDoc.root().attr("lang").value(),
-				Table_Schema: xmlDoc.root().attr("Table_Schema").value(),
-				Table_Name: xmlDoc.root().attr("Table_Name").value(),
-				mode: xmlDoc.root().attr("mode").value(),
-				controlType: xmlDoc.root().attr("controlType").value()
-			};
+			var xsl_path = path.join(__dirname, '..', 'xsl', req.query.output + '.xsl');
 
-			var sLocation = path.join(
-				panax.ui.guis[output].root,
-				"cache", "app",
-				catalog.dbId,
-				catalog.lang,
-				catalog.Table_Schema,
-				catalog.Table_Name,
-				catalog.mode
-			);
+			libxslt.parseFile(xsl_path, function (err, stylesheet) {
+				if (err)
+					return next(err);
 
-			var sFileName = path.join(sLocation, catalog.controlType + '.js');
-
-			// ToDo: Use Async functions?
-			if(fs.existsSync(sFileName) && !req.query.rebuild) {
-				console.info('# /api/build - Already existing file: ' + sFileName);
-				res.json({
-					success: true,
-					action: "existing",
-					filename: sFileName,
-					catalog: catalog
-				});
-			} else {
-				if(fs.existsSync(sFileName)) {
-					fs.unlinkSync(sFileName);
-					console.info('# /api/build - Deleted file: ' + sFileName);
-				}
-
-				console.info('# /api/build - Building file: ' + sFileName);
-
-				if(!fs.existsSync(sLocation)) {
-					mkdirp(sLocation);
-					console.info('# /api/build - Mkdirp folder: ' + sLocation);
-				}
-
-				libxslt.parseFile('xsl/' + output + '.xsl', function (err, stylesheet) {
-					if (err)
+				stylesheet.apply(xml, function (err, result) {
+					if (err) 
 						return next(err);
 
-					stylesheet.apply(xml, function (err, result) {
+					fs.writeFile(sFileName, result, function (err) {
 						if (err) 
 							return next(err);
-
-						fs.writeFile(sFileName, result, function (err) {
-							if (err) 
-								return next(err);
-							// CONSOLE.LOG Created file: sFileName
-							res.json({
-								success: true,
-								action: "built",
-								filename: sFileName,
-								catalog: catalog
-							});
+						// CONSOLE.LOG Created file: sFileName
+						res.json({
+							success: true,
+							action: "built",
+							filename: sFileName,
+							catalog: catalog
 						});
 					});
 				});
-			}
-		});
+			});
+		}
 	});
 });
--- routes/api_read.js
+++ routes/api_read.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var sql = require('mssql');
 var PanaxJS = require('../../PanaxJS/index');
-var panax = require('../config/panax');
+var panax_config = require('../config/panax');
 
 var fs = require('fs');
 var libxslt = require('libxslt');
@@ -24,88 +23,70 @@ router.get('/', function read(req, res, next) {
 	if (!req.query.catalogName)
 		return next({message: "Error: No catalogName supplied"});
 
-	var output = (req.query.output || 'json').toLowerCase(); // JSON is default output
+	req.query.output = (req.query.output || 'json').toLowerCase(); // JSON is default output
 
-	if (output != 'json' && output != 'html')
-		return next({message: "Error: Output '" + output + "' not supported"});
+	if (req.query.output != 'json' && req.query.output != 'html')
+		return next({message: "Error: Output '" + req.query.output + "' not supported"});
 
-	sql.connect(panax.db.config, function (err) {
-		if (err)
-			return next(err);
+	var oPanaxJS = new PanaxJS(req.query);
 
-		var sql_req = new sql.Request();
+	oPanaxJS.set('userId', req.session.userId);
+	oPanaxJS.set('tableName', req.query.catalogName);
+	/**/oPanaxJS.set('output', req.query.output);
+	oPanaxJS.set('getData', (req.query.getData || '1'));
+	oPanaxJS.set('getStructure', (req.query.getStructure || '0'));
+	oPanaxJS.set('lang', (req.session.lang || 'DEFAULT'));
 
-		var oPanaxJS = new PanaxJS(req.query);
-		oPanaxJS.set('userId', req.session.userId);
-		oPanaxJS.set('tableName', req.query.catalogName);
-		oPanaxJS.set('output', output);
-		oPanaxJS.set('getData', (req.query.getData || '1'));
-		oPanaxJS.set('getStructure', (req.query.getStructure || '0'));
-		oPanaxJS.set('lang', (req.session.lang || 'DEFAULT'));
+	oPanaxJS.getXML(panax_config.db.config, function (err, xml) {
+		var xmlDoc = libxslt.libxmljs.parseXml(xml);
 
-		var sql_str = oPanaxJS.toSQLString();
+		if(!xmlDoc)
+			return next({message: "Error: Parsing XML"});
 
-		sql_req.query(sql_str, function (err, recordset) {
-			if (err)
-				return next(err);
+		if (xmlDoc.root().attr("controlType").value() == 'fileTemplate') {
 
-			console.info('# /api/read - sql_str: ' + sql_str);
+			var fileTemplate = xmlDoc.root().attr("fileTemplate");
 
-			var xml = recordset[0][''];
+			if(!fileTemplate)
+				return next({message: "Error: Missing fileTemplate"});
 
-			if(!xml)
-				return next({message: "Error: Missing Data XML"});
-
-			var xmlDoc = libxslt.libxmljs.parseXml(xml);
-
-			if(!xmlDoc)
-				return next({message: "Error: Parsing XML"});
-
-			if (xmlDoc.root().attr("controlType").value() == 'fileTemplate') {
-
-				var fileTemplate = xmlDoc.root().attr("fileTemplate");
-
-				if(!fileTemplate)
-					return next({message: "Error: Missing fileTemplate"});
-
-				try {
-					pate.parse({
-						tpl: fs.readFileSync('templates/' + req.query.catalogName + '/' + fileTemplate.value()),
-						xml: xml,
-						xpath: '/*/px:data/px:dataRow',
-						ns: {
-							px: 'urn:panax'
-						},
-						format_lib: formatter
-					}, function (err, result) {
-						// ToDo: Should handle content-type according to template file (ex. SVG)
-						res.set('Content-Type', 'text/html');
-						res.send(result);
-					});
-				} catch (e) {
-					return next({
-						message: '[Server Exception] ' + e.name + ': ' + e.message,
-						stack: e.stack
-					});
-				}
-			} else {
-				libxslt.parseFile('xsl/' + output + '.xsl', function (err, stylesheet) {
-					if (err)
-						return next(err);
-
-					stylesheet.apply(xml, function (err, result) {
-						if (err) 
-							return next(err);
-
-						if (output == 'json') {
-							res.json(JSON.parse(util.sanitizeJSONString(result)));
-						} else if (output == 'html') {
-							res.set('Content-Type', 'text/html');
-							res.send(result);
-						}
-					});
+			try {
+				pate.parse({
+					tpl: fs.readFileSync('templates/' + req.query.catalogName + '/' + fileTemplate.value()),
+					xml: xml,
+					xpath: '/*/px:data/px:dataRow',
+					ns: {
+						px: 'urn:panax'
+					},
+					format_lib: formatter
+				}, function (err, result) {
+					// ToDo: Should handle content-type according to template file (ex. SVG)
+					res.set('Content-Type', 'text/html');
+					res.send(result);
+				});
+			} catch (e) {
+				return next({
+					message: '[Server Exception] ' + e.name + ': ' + e.message,
+					stack: e.stack
 				});
 			}
-		});
+		} else {
+			libxslt.parseFile('xsl/' + req.query.output + '.xsl', function (err, stylesheet) {
+				if (err)
+					return next(err);
+
+				stylesheet.apply(xml, function (err, result) {
+					if (err) 
+						return next(err);
+
+					if (req.query.output == 'json') {
+						res.json(JSON.parse(util.sanitizeJSONString(result)));
+					} else if (req.query.output == 'html') {
+						res.set('Content-Type', 'text/html');
+						res.send(result);
+					}
+				});
+			});
+		}
 	});
 });
