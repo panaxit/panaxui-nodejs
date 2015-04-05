83cd47823ffc16c279f11bf04b85c215c2211d34
Benjamin Orozco <benoror@gmail.com>
2015-04-05 11:49:13 -0500

    CRUD API: Delete (DELETE /api/delete)

--- routes/api_delete.js
+++ routes/api_delete.js
@@ -4,6 +4,7 @@ var Panax = require('panaxjs');
 var config = require('../config/panax.js');
 
 var auth = require('../lib/auth.js');
+var xml = require('../lib/xml.js');
 
 module.exports = router;
 
@@ -13,5 +14,38 @@ module.exports = router;
  * Delete Entity
  */
 router.delete('/', auth.requiredAuth, function read(req, res, next) {
-	
+	if (!req.body.tableName)
+		return next({message: "No tableName supplied"});
+	if (!req.body.primaryKey || !req.body.identityKey)
+		return next({message: "No primaryKey or identityKey supplied"});
+	if (!req.body.deleteRows || !req.body.deleteRows.length || req.body.deleteRows.length===0)
+		return next({message: "No deleteRows supplied"});
+
+	/**
+	 * Build dataTable XML
+	 */
+	var deleteXML = xml.buildDataTable(req.body);
+
+	/**
+	 * PanaxJS
+	 */
+	var oPanax = new Panax(config);
+
+	oPanax.setParam('userId', req.session.userId);
+
+	oPanax.updateDB(deleteXML, function (err, xml) {
+		if(err)
+			return next(err);
+
+		oPanax.getResults(xml, function (err, results) {
+			if(err)
+				return next(err);
+
+			res.json({
+				success: true,
+				action: 'delete',
+				data: results
+			});
+		});
+	});
 });
