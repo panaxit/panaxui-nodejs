2b085d2f73f5608c1d6680f834839b90fa999c4a
Benjamin Orozco <benoror@gmail.com>
2015-07-03 11:44:11 -0500

    test(cards): add passing test for cardView/readonly

--- test/integration/ng/read.js
+++ test/integration/ng/read.js
@@ -119,6 +119,38 @@ describe('read', function() {
 			});
 		});
 
+		it('should read cardView/readonly', function(done) {
+			var query = querystring.stringify({
+				gui: 'ng',
+				output: "json",
+				catalogName: "TestSchema.CONTROLS_Basic",
+				controlType: 'cardView',
+				mode: 'readonly'
+			});
+
+			api.get('/api/read?' + query)
+			.set('Accept', 'application/json')
+			.set('Cookie', cookie) // Pass session cookie with each request
+			.expect(200)
+			.expect('Content-Type', /json/)
+			.end(function(err, res) {
+				if (err) return done(err);
+				expect(res.body.success).to.be.true;
+				expect(res.body.action).to.equal('read');
+				expect(res.body.gui).to.equal('ng');
+				expect(res.body.output).to.equal('json');
+				expect(res.body.data.total).to.equal('1');
+				expect(res.body.data.model.length).to.be.above(0);
+				expect(res.body.data.catalog.dbId).to.equal(config.db.database);
+				expect(res.body.data.catalog.catalogName).to.equal('TestSchema.CONTROLS_Basic');
+				expect(res.body.data.catalog.controlType).to.equal('cardView');
+				expect(res.body.data.catalog.mode).to.equal('readonly');
+				expect(res.body.data.catalog.primaryKey).to.equal('Id');
+				expect(res.body.data.catalog.identityKey).to.equal('Id');
+				done();
+			});
+		});
+
 		it('should read formView/readonly', function(done) {
 			var query = querystring.stringify({
 				gui: 'ng',
