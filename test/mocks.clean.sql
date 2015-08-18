613669a53cbb137e2d297d3337bf36f948f4065b
Benjamin Orozco <benoror@gmail.com>
2015-08-18 12:26:31 -0500

    test(mocks.clean.sql): clear cache before deleting tables

--- test/mocks.clean.sql
+++ test/mocks.clean.sql
@@ -1,11 +1,23 @@
 if object_id('[TestSchema].[CONTROLS_Basic]', 'U') is not null
-		exec('drop table [TestSchema].[CONTROLS_Basic]')
+begin
+	exec('drop table [TestSchema].[CONTROLS_Basic]');
+	exec [$Ver:Beta_12].clearCache '[TestSchema].[CONTROLS_Basic]';
+end
 
 if object_id('[TestSchema].[CONTROLS_NestedGrid]', 'U') is not null
-		exec('drop table [TestSchema].[CONTROLS_NestedGrid]')
+begin
+	exec('drop table [TestSchema].[CONTROLS_NestedGrid]');
+	exec [$Ver:Beta_12].clearCache '[TestSchema].[CONTROLS_NestedGrid]';
+end
 
 if object_id('[TestSchema].[CONTROLS_NestedForm]', 'U') is not null
-		exec('drop table [TestSchema].[CONTROLS_NestedForm]')
+begin
+	exec('drop table [TestSchema].[CONTROLS_NestedForm]');
+	exec [$Ver:Beta_12].clearCache '[TestSchema].[CONTROLS_NestedForm]';
+end
 
 if object_id('[TestSchema].[Pais]', 'U') is not null
-		exec('drop table [TestSchema].[Pais]')
+begin
+	exec('drop table [TestSchema].[Pais]');
+	exec [$Ver:Beta_12].clearCache '[TestSchema].[Pais]';
+end
