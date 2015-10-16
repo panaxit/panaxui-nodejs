63983f3eb5b0cac8368291fc8587679c65d86df2
Benjamin Orozco <benoror@gmail.com>
2015-10-16 13:41:38 -0500

    test(trans[json.fields.px-form]): basic layout failing tests

--- test/unit/transformers/ng/json.fields.px-form.js
+++ test/unit/transformers/ng/json.fields.px-form.js
@@ -1,8 +1,95 @@
 var expect = require('chai').expect;
 var Fields = require('../../../../transformers/ng/json.fields');
 
-describe('px-form', function() {
+describe.skip('px-form', function() {
 
-	it('PENDING');
+	describe('basic', function() {
+
+		it('empty layout should return array with empty fieldset', function() {
+			var xml = '<Entity xmlns:px="urn:panax" controlType="formView">' +
+								'	<px:layout>' +
+								'	</px:layout>' +
+								'</Entity>';
+			var result = Fields.Transform(xml);
+			expect(result).to.deep.equal(
+				[
+					{
+						"type": "fieldset",
+						"fields": []
+					}
+				]
+			);
+		});
+		
+	});
+
+	describe('fieldset / fieldContainer', function() {
+		
+		it('PENDING');
+
+	});
+
+	describe('tabPanel / tabs', function() {
+
+		it('layout with empty tabpanel should return empty tabs', function() {
+			var xml = '<Entity xmlns:px="urn:panax" controlType="formView">' +
+								'	<px:layout>' +
+								'		<px:tabPanel>' +
+								'		</px:tabPanel>' +
+								'	</px:layout>' +
+								'</Entity>';
+			var result = Fields.Transform(xml);
+			expect(result).to.deep.equal(
+				[
+					{
+						"type": "fieldset",
+						"fields": [
+							{
+								"type": "tabPanel",
+								"tabs": []
+							}
+						]
+					}
+				]
+			);
+		});
+
+		it('layout with empty tabs should return named tabs with empty fields', function() {
+			var xml = '<Entity xmlns:px="urn:panax" controlType="formView">' +
+								'	<px:layout>' +
+								'		<px:tabPanel>' +
+								'			<px:tab name="Tab 1">' +
+								'			</px:tab>' +
+								'			<px:tab name="Tab 2">' +
+								'			</px:tab>' +
+								'		</px:tabPanel>' +
+								'	</px:layout>' +
+								'</Entity>';
+			var result = Fields.Transform(xml);
+			expect(result).to.deep.equal(
+				[
+					{
+						"type": "fieldset",
+						"fields": [
+							{
+								"type": "tabPanel",
+								"tabs": [
+									{"type": "tab", "name": "Tab 1", "fields": []},
+									{"type": "tab", "name": "Tab 2", "fields": []}
+								]
+							}
+						]
+					}
+				]
+			);
+		});
+		
+	});
+
+	describe('fields', function() {
+		
+		it('PENDING');
+		
+	});
 	
 });
