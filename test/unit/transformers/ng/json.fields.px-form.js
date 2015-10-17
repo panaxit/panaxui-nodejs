bca6d8be3cac22bb2ec71c068b907eb0b1a88ba6
Benjamin Orozco <benoror@gmail.com>
2015-10-16 19:01:03 -0500

    feat(trans[json.fields.px-form]): advanced layout + data types

--- test/unit/transformers/ng/json.fields.px-form.js
+++ test/unit/transformers/ng/json.fields.px-form.js
@@ -1,7 +1,7 @@
 var expect = require('chai').expect;
 var Fields = require('../../../../transformers/ng/json.fields');
 
-describe.skip('px-form', function() {
+describe('px-form', function() {
 
 	describe('basic', function() {
 
@@ -87,7 +87,173 @@ describe.skip('px-form', function() {
 	});
 
 	describe('fields', function() {
+
+		it('layout with fields should return array of field objects', function() {
+			var xml = '<Entity xmlns:px="urn:panax" controlType="formView">' +
+								'	<px:fields>' +
+								'		<FieldA fieldId="IDA" fieldName="FieldA" />' +
+								'		<FieldB fieldId="IDB" fieldName="FieldB" />' +
+								'	</px:fields>' +
+								'	<px:layout>' +
+								'		<px:tabPanel>' +
+								'			<px:tab name="Tab">' +
+								'				<px:field fieldId="IDA" />' +
+								'				<px:field fieldId="IDB" />' +
+								'			</px:tab>' +
+								'		</px:tabPanel>' +
+								'	</px:layout>' +
+								'</Entity>';
+			var result = Fields.Transform(xml);
+			expect(result).to.deep.equal(
+				[
+					{
+						"type": "fieldset",
+						"fields": [
+							{
+								"type": "tabPanel",
+								"tabs": [
+									{
+										"type": "tab", 
+										"name": "Tab", 
+										"fields": [
+											{
+												"key": "FieldA",
+												"type": "default",
+												"templateOptions": {
+													"label": "",
+													"placeholder": ""
+												},
+												"data": {}
+											},
+											{
+												"key": "FieldB",
+												"type": "default",
+												"templateOptions": {
+													"label": "",
+													"placeholder": ""
+												},
+												"data": {}
+											}
+										]
+									}
+								]
+							}
+						]
+					}
+				]
+			);
+		});
 		
+	});
+
+	describe('data types', function() {
+
+		it('basic data types', function() {
+			var xml = '<Entity xmlns:px="urn:panax" controlType="formView">' +
+								'	<px:fields>' +
+								'		<ShortTextField fieldId="ID00" fieldName="ShortTextField" headerText="Short Text Field" dataType="nvarchar" length="255" controlType="default" />' +
+								'		<IntegerReq fieldId="ID01" fieldName="IntegerReq" headerText="Integer Req" dataType="int" length="10" controlType="default" />' +
+								'		<Float fieldId="ID02" fieldName="Float" headerText="Float" dataType="float" controlType="default" />' +
+								'		<RadioGroup fieldId="ID03" fieldName="RadioGroup" headerText="Radio Group" dataType="foreignKey" relationshipType="belongsTo" length="10" controlType="radiogroup" />' +
+								'		<Combobox fieldId="ID04" fieldName="Combobox" headerText="Combobox" dataType="foreignKey" relationshipType="belongsTo" length="2" controlType="combobox" />' +
+								'		<Boolean fieldId="ID05" fieldName="Boolean" headerText="Boolean" dataType="bit" controlType="default" />' +
+								'		<Money fieldId="ID06" fieldName="Money" headerText="Money" dataType="money" length="15" controlType="default" />' +
+								'		<Date fieldId="ID07" fieldName="Date" headerText="Date" dataType="date" controlType="default" />' +
+								'		<Datetime fieldId="ID08" fieldName="Datetime" headerText="Datetime" dataType="datetime" controlType="default" />' +
+								'		<Time fieldId="ID09" fieldName="Time" headerText="Time" dataType="time" controlType="default" />' +
+								'		<LongText fieldId="ID10" fieldName="LongText" headerText="Long Text" dataType="text" length="2147483647" controlType="default" />' +
+								'	</px:fields>' +
+								'	<px:layout>' +
+								'		<px:field fieldId="ID00" />' +
+								'		<px:field fieldId="ID01" />' +
+								'		<px:field fieldId="ID02" />' +
+								'		<px:field fieldId="ID03" />' +
+								'		<px:field fieldId="ID04" />' +
+								'		<px:field fieldId="ID05" />' +
+								'		<px:field fieldId="ID06" />' +
+								'		<px:field fieldId="ID07" />' +
+								'		<px:field fieldId="ID08" />' +
+								'		<px:field fieldId="ID09" />' +
+								'		<px:field fieldId="ID10" />' +
+								'	</px:layout>' +
+								'</Entity>';
+			var result = Fields.Transform(xml);
+			expect(result).to.deep.equal(
+				[
+					{
+						"type": "fieldset",
+						"fields": [
+							{ "key": "ShortTextField", "type": "input", "templateOptions": { "label": "Short Text Field", "placeholder": "" }, "data": {} },
+							{ "key": "IntegerReq", "type": "number", "templateOptions": { "label": "Integer Req", "placeholder": "" }, "data": {} },
+							{ "key": "Float", "type": "number", "templateOptions": { "label": "Float", "placeholder": "" }, "data": {} },
+							{ "key": "RadioGroup", "type": "radio", "templateOptions": { "label": "Radio Group", "placeholder": "" }, "data": {} },
+							{ "key": "Combobox", "type": "async_select", "templateOptions": { "label": "Combobox", "placeholder": "" }, "data": {} },
+							{ "key": "Boolean", "type": "checkbox", "templateOptions": { "label": "Boolean", "placeholder": "" }, "data": {} },
+							{ "key": "Money", "type": "money", "templateOptions": { "label": "Money", "placeholder": "" }, "data": {} },
+							{ "key": "Date", "type": "date", "templateOptions": { "label": "Date", "placeholder": "" }, "data": {} },
+							{ "key": "Datetime", "type": "datetime", "templateOptions": { "label": "Datetime", "placeholder": "" }, "data": {} },
+							{ "key": "Time", "type": "time", "templateOptions": { "label": "Time", "placeholder": "" }, "data": {} },
+							{ "key": "LongText", "type": "textarea", "templateOptions": { "label": "Long Text", "placeholder": "" }, "data": {} }
+						]
+					}
+				]
+			);
+		});
+
+		it('advanced data types', function() {
+			var xml = '<Entity xmlns:px="urn:panax" controlType="formView">' +
+								'	<px:fields>' +
+								'		<EMail fieldId="ID00" fieldName="EMail" headerText="E Mail" dataType="nvarchar" length="255" controlType="email" />' +
+								'		<Color fieldId="ID01" fieldName="Color" headerText="Color" dataType="nchar" length="7" controlType="color" />' +
+								'		<PxFile fieldId="ID02" fieldName="PxFile" headerText="Px File" dataType="nvarchar" length="255" controlType="file" />' +
+								'		<PxPicture fieldId="ID03" fieldName="PxPicture" headerText="Px Picture" dataType="nvarchar" length="255" controlType="picture" />' +
+								'		<PxPassword fieldId="ID04" fieldName="PxPassword" headerText="Px Password" dataType="nchar" length="32" controlType="password" />' +
+								'	</px:fields>' +
+								'	<px:layout>' +
+								'		<px:field fieldId="ID00" />' +
+								'		<px:field fieldId="ID01" />' +
+								'		<px:field fieldId="ID02" />' +
+								'		<px:field fieldId="ID03" />' +
+								'		<px:field fieldId="ID04" />' +
+								'	</px:layout>' +
+								'</Entity>';
+			var result = Fields.Transform(xml);
+			expect(result).to.deep.equal(
+				[
+					{
+						"type": "fieldset",
+						"fields": [
+							{ "key": "EMail", "type": "email", "templateOptions": { "label": "E Mail", "placeholder": "" }, "data": {} },
+							{ "key": "Color", "type": "color", "templateOptions": { "label": "Color", "placeholder": "" }, "data": {} },
+							{ "key": "PxFile", "type": "file", "templateOptions": { "label": "Px File", "placeholder": "" }, "data": {} },
+							{ "key": "PxPicture", "type": "file", "templateOptions": { "label": "Px Picture", "placeholder": "" }, "data": {} },
+							{ "key": "PxPassword", "type": "password", "templateOptions": { "label": "Px Password", "placeholder": "" }, "data": {} }
+						]
+					}
+				]
+			);
+		});
+		
+	});
+
+	describe('foreign tables', function() {
+
+		describe('hasOne', function() {
+
+			it('PENDING');
+			
+		});
+
+		describe('hasMany', function() {
+
+			it('PENDING');
+			
+		});
+		
+	});
+
+	describe('junction tables', function() {
+
 		it('PENDING');
 		
 	});
--- transformers/ng/json.fields.px-form.js
+++ transformers/ng/json.fields.px-form.js
@@ -5,5 +5,226 @@ Helpers
  */
 var _attr = require('../helpers').attr;
 var _el = require('../helpers').el;
+var _keyIndex = require('../helpers').keyIndex;
 
-// ToDo
+/*
+Keys & Indexes
+ */
+var $_FieldsIndex;
+
+/*
+Main entry point
+ */
+var _Main = exports;
+
+_Main.Transform = function(Entity) {
+	var Layout = _el.get(Entity, 'px:layout');
+
+	$_FieldsIndex = _keyIndex(Entity, "px:fields/*|px:fields//*[@fieldId][not(namespace-uri(.)='urn:panax')]", 'fieldId');
+
+	return _Main.Layout(Layout);
+};
+
+_Main.Layout = function(Layout) {
+	var Children = _el.find(Layout, '*');
+	return [_Main.FieldSet(Children)];
+};
+
+_Main.FieldSet = function(Fields) {
+	return {
+		"type": "fieldset",
+		"fields": _Main.Fields(Fields)
+	};
+};
+
+/*
+ToDo:
+@fieldContainer (fieldGroup)
+		.orentation = horizontal / vertical
+ */
+
+_Main.Fields = function(Fields) {
+	var fields = [];
+	Fields.forEach(function (Field, index) {
+		var name = _el.name(Field);
+		switch(name) {
+			case 'field':
+				fields.push(_Main.Field(Field));
+				break;
+			case 'tabPanel':
+				fields.push(_Main.TabPanel(Field));
+				break;
+			default:
+				fields.push({});
+				break;
+		};
+	});
+	return fields;
+};
+
+_Main.TabPanel = function(TabPanel) {
+	var Tabs = _el.find(TabPanel, 'px:tab');
+	return {
+		"type": "tabPanel",
+		"tabs": _Main.Tabs(Tabs)
+	};
+};
+
+_Main.Tabs = function(Tabs) {
+	var tabs = [];
+	Tabs.forEach(function (Tab, index) {
+		tabs.push(_Main.Tab(Tab));
+	});
+	return tabs;
+};
+
+_Main.Tab = function(Tab) {
+	var Children = _el.find(Tab, '*');
+	return {
+		"type": "tab",
+		"name": _attr.val(Tab, 'name'),
+		"fields": _Main.Fields(Children)
+	};
+};
+
+_Main.Field = function(Field) {
+	var fieldId = _attr.val(Field, 'fieldId');
+	var Metadata = $_FieldsIndex[fieldId];
+	return {
+		"key": _attr.val(Metadata, 'fieldName'), // _el.name(Metadata)
+		"type": _Main.Type(Metadata),
+		"templateOptions": {
+			"label": _attr.val(Metadata, 'headerText') || '',
+			"placeholder": ""
+		},
+		"data": {}
+	};
+};
+
+_Main.Type = function(Metadata) {
+	var dataType = _attr.val(Metadata, 'dataType');
+
+	switch(dataType) {
+		default:
+			return _Main.regularFieldsTypes(Metadata);
+		case 'foreignTable':
+			return _Main.foreignTableTypes(Metadata);
+		case 'junctionTable':
+			return _Main.junctionTableTypes(Metadata);
+	}
+};
+
+_Main.regularFieldsTypes = function(Metadata) {
+	var dataType = _attr.val(Metadata, 'dataType');
+	var controlType = _attr.val(Metadata, 'controlType');
+	var length = _attr.val(Metadata, 'length');
+
+	switch(controlType) {
+		case 'email':
+			return 'email';
+		case 'password':
+			return 'password';
+		case 'color':
+			return 'color';
+		case 'picture':
+		case 'file':
+			return 'file';
+		default:
+			return 'default';
+		case 'radiogroup': {
+			switch(dataType) {
+				case 'foreignKey':
+					return 'radio';
+				default:
+					return 'radio';
+			}
+		}
+		case 'combobox': {
+			switch(dataType) {
+				case 'foreignKey':
+					return 'async_select';
+				default:
+					return 'async_select';
+			}
+		}
+		case 'default': {
+			switch(dataType) {
+				case 'char':
+					return 'input';
+				case 'varchar':
+				case 'nvarchar':
+				case 'nchar':
+				case 'text': {
+					if(!length || parseInt(length)<=255)
+						return 'input';
+					else
+						return 'textarea';
+				}
+				case 'int':
+				case 'tinyint':
+				case 'float':
+					return 'number';
+				case 'money':
+					return 'money';
+				case 'date':
+					return 'date';
+				case 'time':
+					return 'time';
+				case 'datetime':
+					return 'datetime';
+				case 'bit':
+					return 'checkbox';
+				case 'foreignKey':
+					return 'async_select';
+				default:
+					return 'default';
+			}
+		}
+	}
+};
+
+_Main.foreignTableTypes = function(Metadata) {
+	var relationshipType = _attr.val(Metadata, 'relationshipType');
+	var controlType = _attr.val(Metadata, 'controlType');
+
+	switch(relationshipType) {
+		default:
+		case 'hasOne': {
+			switch(controlType) {
+				case 'default':
+				case 'formView':
+				default:
+					return 'form';
+			}
+		}
+		case 'hasMany': {
+			switch(controlType) {
+				case 'default':
+				case 'gridView':
+				default:
+					return 'grid';
+				case 'cardsView':
+					return 'cards';
+				case 'formView':
+					return 'form';
+			}
+		}
+	}
+};
+
+_Main.junctionTableTypes = function(Metadata) {
+	var relationshipType = _attr.val(Metadata, 'relationshipType');
+	var controlType = _attr.val(Metadata, 'controlType');
+
+	switch(relationshipType) {
+		default:
+		case 'hasMany': {
+			switch(controlType) {
+				case 'default':
+				case 'gridView':
+				default:
+					return 'junction';
+			}
+		}
+	}
+};
